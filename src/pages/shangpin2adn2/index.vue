00206728d7e1079b23a868bc177e3c9e6e4e9a8d
1264758448 <1264758448@qq.com>
2019-02-16 21:45:55 +0800

    小球动画

---
+++ src/pages/shangpin2adn2/index.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <pl :id='id'></pl>
+    </div>
+</template>
+<script>
+export default {
+    data(){
+        return{
+            id:this.$route.params.id
+        }
+    }
+}
+</script>
